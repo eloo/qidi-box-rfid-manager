f222018077ec7d16e6a2b4707b3a27ceace26488
Joseph Weigl <joseph.weigl@gmail.com>
2026-03-21 14:40:50 +0100

    style: format code and improve readability in manufacturer.ts

--- constants/manufacturer.ts
+++ constants/manufacturer.ts
@@ -1,9 +1,9 @@
-import type { Manufacturer } from '@/types';
+import type { Manufacturer } from "@/types";
 
 /**
  * Manufacturer codes for QIDI RFID tags
  * Based on official QIDI Wiki: https://wiki.qidi3d.com/en/QIDIBOX/RFID
- * 
+ *
  * The manufacturer code is stored in bit[2] of the RFID data
  * Default value from factory is 1 (QIDI)
  * Supports values 0-255 (1 byte)
@@ -11,9 +11,12 @@ import type { Manufacturer } from '@/types';
 export const MANUFACTURERS: Manufacturer[] = [
   { code: 0, name: "Generic" },
   { code: 1, name: "QIDI" },
+  { code: 2, name: "Sunlu" },
 ];
 
-export const getManufacturerByCode = (code: number): Manufacturer | undefined => {
+export const getManufacturerByCode = (
+  code: number,
+): Manufacturer | undefined => {
   return MANUFACTURERS.find((m: Manufacturer) => m.code === code);
 };
 
